1fa4bb159096b10633c44a21f602138452074f27
Kaique Souza <kaique.souza@picpay.com>
2025-12-26 13:25:46 -0300

    feat(src/main.jsx)!: migrating to LenisProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,32 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProjectsPage from './ProjectsPage';
+import { LenisProvider } from './hooks/useLenis.jsx';
 
-import Lenis from 'lenis';
 import './styles/global.css'
 import './styles/mobile.css'
 
 
-const lenis = new Lenis({
-  lerp: 0.055,
-  smooth: true,
-  wheelMultiplier: 0.8,
-  touchMultiplier: 0.5  
-});
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/projects" element={<ProjectsPage />} />
-    </Routes>
+    <LenisProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </LenisProvider>
   </BrowserRouter>
 )
